797bd95de3bfe9a07649862d2643ef8017a218a4
Nicholas Doyle <Nic.Doyle15@gmail.com>
2014-03-16 17:30:23 -0700

    added a shuriken model with textures, also added the statusEffect script as well as a playerManager Script. Some weapon functionality is built into the playerManager Script.

---
+++ unity/Assets/scripts/ScriptsFromNic/PlayerManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour {
+
+	int status;
+	
+	// Player Attribute variables
+	public float health;
+	public float dodgeCoolDown;
+	public List<Weapon> toolBelt;
+	
+	//{ **Bools for status Effects** 
+	public bool burning;
+	public bool chilled;
+	public bool shocked;
+	public bool poisoned;
+	public bool blinded;
+	//}
+	public List<StatusEffects> statusEffectsOnPlayer;
+	
+	/*elemental effects
+		0 = Burn
+		1 = Blind
+		2 = Freeze
+		3 = Shock
+		4 = Poison
+	*/
+	
+	//{ **Methods for adding Status Effect to Players**
+	public void Burn(float baseDamage){statusEffectsOnPlayer.Add (new BurnEffect(baseDamage));}
+	public void Blind(float duration){statusEffectsOnPlayer.Add(new BlindEffect(duration));}
+	public void Freeze(float duration){statusEffectsOnPlayer.Add(new FrostEffect(duration));}
+	public void Shock(float duration){statusEffectsOnPlayer.Add(new ShockEffect(duration));}
+	public void Poison(float baseDamage){statusEffectsOnPlayer.Add (new BurnEffect(baseDamage));}
+	//}
+	
+	void Start () {
+		statusEffectsOnPlayer = new List<StatusEffects>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//updates all statuses on player if they exist
+		foreach(StatusEffects status in statusEffectsOnPlayer)
+		{
+			status.Update();
+			if(status is BurnEffect)
+			{
+				burning = true;
+				health -= status.dps * Time.deltaTime;
+			}
+			if(status is FrostEffect)
+			{
+				chilled = true;
+			}
+			if(status is ShockEffect)
+			{
+				shocked = true;
+			}
+			if(status is PoisonEffect)
+			{
+				poisoned = true;
+			}
+			if(status is BlindEffect)
+			{
+				blinded = true;
+			}
+		}
+
+	}
+	public void Attack()
+	{
+		//if left hand
+		//if right hand
+	}
+	public void loadWeapon()
+	{
+		//loads weapon of type into empty weapon slot
+	}
+}
+public class Weapon : MonoBehaviour
+{
+	//skillTree Effects on all weapons
+
+	public float atkSpd;
+	public float pwrAtkActivateSpd;
+	public float pwrtAtkCoolDown;
+	public float pwrAtkDmg;
+	public float nrmAtkDmg;
+	public float critDmgBns;
+	public float specEffectivePrcnt;
+	public float procChnce;
+	
+}
+public class Melee:Weapon
+{
+	public float critWindowRnge;
+	public enum elemType{shadow,fire,ice,lightning,poison};
+	elemType element;
+}
+public class Ranged:Weapon
+{
+	public float chnceToNotConsumeCombo;
+	public enum elemType{shadow,fire,ice,lightning,poison};
+	elemType element;
+}
+public class Spell:Weapon
+{
+	public float comboTmrLngth;
+
+	public enum elemType{shadow,fire,ice,lightning};
+	elemType primary;
+	elemType secondary;
+
+	public bool didItCrit()
+	{
+		return procChnce>=Random.Range (0.0f,100.0f);
+	}
+}
---
+++ unity/Assets/scripts/ScriptsFromNic/StatusEffects.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusEffects  {
+	public GameObject playerObject;
+	public float dps;
+	
+	public float elapsedTime;
+	public int count;
+	
+	// Update is called once per frame
+	public virtual void Update () {
+	
+	}
+}
+public class BurnEffect : StatusEffects {
+	int duration ;
+	public BurnEffect (float baseDamage) 
+	{
+		elapsedTime = 0;
+		duration = 5;
+		dps = (baseDamage * 0.1f) / duration;
+		count = 1;
+	}
+	
+	// Update is called once per frame
+	public override void Update () 
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime >= count)
+		{
+			count++;
+			playerObject.GetComponent<PlayerManager>().health -= dps;
+		}
+		if(elapsedTime >= duration)
+			playerObject.GetComponent<PlayerManager>().statusEffectsOnPlayer.Remove(this);
+	}
+}
+public class FrostEffect : StatusEffects
+{
+	float duration;
+	public FrostEffect (float _duration) 
+	{
+		elapsedTime = 0;
+		duration = _duration;
+		dps = 0;
+		count = 1;
+		playerObject.GetComponent<PlayerManager>().Freeze(duration);
+	}
+	
+	// Update is called once per frame
+	public override void Update () 
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime >= count)
+			count++;
+		if(elapsedTime >= duration)
+			playerObject.GetComponent<PlayerManager>().statusEffectsOnPlayer.Remove(this);
+	}
+}
+public class BlindEffect : StatusEffects {
+	float duration;
+	public BlindEffect(float _duration)
+	{
+		elapsedTime = 0;
+		duration = _duration;
+		count = 1;
+		playerObject.GetComponent<PlayerManager>().Blind(duration);
+	}
+	
+	// Update is called once per frame
+	public override void Update ()
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime >= count)
+			count++;
+		if(elapsedTime >= duration)
+			playerObject.GetComponent<PlayerManager>().statusEffectsOnPlayer.Remove(this);
+	}
+}
+public class ShockEffect : StatusEffects {
+	float duration;
+	public ShockEffect(float _duration) 
+	{
+		elapsedTime = 0;
+		duration = _duration;
+		dps = 3;
+		count = 1;
+		playerObject.GetComponent<PlayerManager>().Shock(duration);
+	}
+	
+	// Update is called once per frame
+	public override void Update ()
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime >= count)
+		{
+			count++;
+			playerObject.GetComponent<PlayerManager>().health -= dps;
+		}
+		if(elapsedTime >= duration)
+			playerObject.GetComponent<PlayerManager>().statusEffectsOnPlayer.Remove(this);
+	}
+}
+public class PoisonEffect : StatusEffects {
+
+	int duration;
+	public PoisonEffect(float baseDamage)
+	{
+		elapsedTime = 0;
+		dps = (baseDamage * 0.05f) + 1;
+		duration = 5;
+		count = 1;
+		playerObject.GetComponent<PlayerManager>().Poison(baseDamage);
+	}
+	
+	//	IEnumerator killDuration()
+	//	{
+	//		playerObject.GetComponent<PlayerManager>().health -= dps;
+	//
+	//		yield return new WaitForSeconds(1);
+	//
+	//	}
+	// Update is called once per frame
+	public override void Update () 
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime >= count)
+		{
+			count++;
+			playerObject.GetComponent<PlayerManager>().health -= dps;
+		}
+		if(elapsedTime >= duration)
+			playerObject.GetComponent<PlayerManager>().statusEffectsOnPlayer.Remove(this);
+		
+	}
+}
