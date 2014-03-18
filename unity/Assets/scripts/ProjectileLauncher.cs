6d7a86cf28f5ef1c830899f1b7d99f6d833f86c3
fueltherage <wesleyallard1990@gmail.com>
2014-03-18 12:20:08 -0400

    Fixed Duplicate Scripts

--- unity/Assets/scripts/ProjectileLauncher.cs
+++ unity/Assets/scripts/ProjectileLauncher.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ProjectileLauncher : MonoBehaviour {
 
 	public GameObject projectile;
+
+	public List<GameObject> weapons;
+
+	private int currentWeaponEquipped=0;
+
 	public float ReloadSpeed = 1;
 	public float ChargeRate = 0.1f;
+	public float predShurikenForce=0;
+	public float predShurikenSpin =0;
 
 	GameObject chargingSpell;
 	float scale=0.1f;
@@ -20,6 +28,7 @@ public class ProjectileLauncher : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		#region networking
 		if(DataGod.currentGameState == DataGod.GameMode.NetWorkPlay)
 		{
 			if(networkView.isMine)
@@ -61,15 +70,40 @@ public class ProjectileLauncher : MonoBehaviour {
 				}
 				reloadTime += Time.deltaTime;
 			}
+			#endregion
+		#region DemoMode
 		}else if(DataGod.currentGameState == DataGod.GameMode.Demo)
 		{
+			if(Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				currentWeaponEquipped = 0;
+			}
+			if(Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				currentWeaponEquipped = 1;
+			}
+			if(Input.GetKeyDown(KeyCode.Alpha3))
+			{
+				currentWeaponEquipped = 2;
+			}
+
+
 			if(reloadTime > ReloadSpeed)
 			{
 				
 				if(Input.GetMouseButtonDown(0))
 				{	
-					chargingSpell = Instantiate(projectile, this.transform.position, Quaternion.LookRotation(transform.forward)) as GameObject;
-					chargingSpell.transform.parent = this.transform;
+					chargingSpell = Instantiate(weapons[currentWeaponEquipped], this.transform.position, Quaternion.LookRotation(transform.forward)) as GameObject;
+					if(chargingSpell.gameObject.tag == "projctile")
+					{
+						chargingSpell.transform.parent = this.transform;
+					}
+					if(chargingSpell.gameObject.tag == "predShuriken")
+					{
+						chargingSpell.gameObject.GetComponent<ShurikenScript>().addForce(predShurikenForce, transform.forward);
+						chargingSpell.gameObject.GetComponent<ShurikenScript>().addRotationalForce(predShurikenSpin,1);
+					}
+
 					MouseJustPressed = true;
 					
 				}
@@ -86,20 +120,27 @@ public class ProjectileLauncher : MonoBehaviour {
 				{
 					MouseJustPressed = false;
 					chargingSpell.transform.parent = null;
-					chargingSpell.AddComponent<Rigidbody>();
-					chargingSpell.GetComponent<SphereCollider>().enabled = true;
+					if(chargingSpell.gameObject.tag == "projectile")
+					{
+						chargingSpell.AddComponent<Rigidbody>();
+						chargingSpell.GetComponent<SphereCollider>().enabled = true;
+						Debug.Log(scale);
+						if(scale>1)scale=1;
+						chargingSpell.rigidbody.useGravity=false;
+						chargingSpell.rigidbody.velocity = this.transform.parent.parent.rigidbody.velocity;
+						chargingSpell.rigidbody.AddForce(transform.parent.parent.GetComponent<Rigidbody>().velocity+this.transform.forward*((3000f*scale)+300));
+						scale = 0.1f;
+
+					}
+
 					//if(scale<=1)chargingSpell.GetComponent<Projectile>().SetScale(scale);
-					Debug.Log(scale);
-					if(scale>1)scale=1;
-					chargingSpell.rigidbody.useGravity=false;
-					chargingSpell.rigidbody.velocity = this.transform.parent.parent.rigidbody.velocity;
-					chargingSpell.rigidbody.AddForce(transform.parent.parent.GetComponent<Rigidbody>().velocity+this.transform.forward*((3000f*scale)+300));
-					scale = 0.1f;
+
 					reloadTime = 0;
 				}
 			}
 			reloadTime += Time.deltaTime;
 		}
+		#endregion
 		
 	}
 	
