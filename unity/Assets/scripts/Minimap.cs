60da05ad295a6c457a7339f8b690e49f6f67ef1a
Epic Verity <epicverity@hotmail.com>
2014-03-22 00:41:55 -0400

    Here is the minimap, but its not perfect!

---
+++ unity/Assets/scripts/Minimap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+	/* public Transform Target;
+	 * 
+	 * void LateUpdate()
+	 * {
+	 * 		transform.position = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+	 * }
+	 * 
+	 *
+	 *
+	 */
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
