83122ae6ae90352229831f68336facc98450497e
fueltherage <wesleyallard1990@gmail.com>
2014-04-11 19:53:48 -0400

    Projectile power attack

---
+++ unity/Assets/FireOffsetting.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireOffsetting : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector3 expansionSpeed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(expansionSpeed,Space.Self
+		                    );
+	}
+}
+
---
+++ unity/Assets/FireSpinRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireSpinRotation : MonoBehaviour {
+
+	public float lifetime;
+	public float rotationSpeed;
+	float elapsedTime=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(0,rotationSpeed,0);
+		elapsedTime+= Time.deltaTime;
+
+		if(elapsedTime>= lifetime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
