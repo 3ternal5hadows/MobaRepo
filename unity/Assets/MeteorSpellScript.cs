08c0a8dd7d3b8d097971daea427ba0b77689fb2c
fueltherage <wesleyallard1990@gmail.com>
2014-04-15 01:07:00 -0400

    Added meteor

--- unity/Assets/MeteorSpellScript.cs
+++ unity/Assets/MeteorSpellScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MeteorSpellScript : MonoBehaviour {
+public class MeteorSpellScript : SpellWeapon {
 
 	// Use this for initialization
 	public GameObject deathEffect;
@@ -15,13 +15,5 @@ public class MeteorSpellScript : MonoBehaviour {
 	void Update () {
 	
 	}
-	void OnCollisionEnter(Collision hit)
-	{
-		if(hit.gameObject.tag == "bounds" || hit.gameObject.tag == "player")
-		{
-			Destroy(this.gameObject,1);
-			rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-			GameObject death = GameObject.Instantiate(deathEffect, this.transform.position, Quaternion.identity) as GameObject;
-		}
-	}
+
 }
